55813f453ac08da7c3fa9435225262f303189e59
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-18 09:31:00 +0200

    feat: initialize join screen component with basic layout and styling

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+
+const join = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>join</Text>
+    </View>
+  );
+};
+
+export default join;
