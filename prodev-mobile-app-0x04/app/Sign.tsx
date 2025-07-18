11ff4ae83af8545fba760f1894fbda59fc77a230
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-18 09:30:44 +0200

    feat: initialize Sign screeen component with basic layout and styling

---
+++ prodev-mobile-app-0x04/app/Sign.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+
+const Sign = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Sign</Text>
+    </View>
+  );
+};
+
+export default Sign;
