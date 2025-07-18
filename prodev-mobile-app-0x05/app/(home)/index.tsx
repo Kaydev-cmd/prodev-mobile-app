67815a468f24400bd965feb7a3e61b8d6bb3e052
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-18 20:07:00 +0200

    feat: implement search bar, filter scroll view, and listing layout on home screen

--- prodev-mobile-app-0x05/app/(home)/index.tsx
+++ prodev-mobile-app-0x05/app/(home)/index.tsx
@@ -1,11 +1,62 @@
+import { FILTERS } from "@/constants";
+import { styles } from "@/styles/_homestyle";
+import { Feather } from "@expo/vector-icons";
 import React from "react";
-import { Text, View } from "react-native";
-const index = () => {
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from "react-native";
+
+const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.searchGroup}>
+        <View style={styles.searchGroupForm}>
+          <View style={styles.searchControlGroup}>
+            <View>
+              <Text style={styles.searchFormText}>Where to?</Text>
+              <TextInput
+                style={{ ...styles.searchControl, ...styles.searchFormText }}
+                placeholder="Location . Date . Add guest"
+              />
+            </View>
+            <View style={styles.searchButton}>
+              <Feather name="search" size={24} color="white" />
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ height: 72, backgroundColor: "white" }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.filterGroup}>
+            {FILTERS.map((filter, index) => (
+              <View style={styles.filterContainer} key={index}>
+                <Image
+                  style={{ flex: 1 }}
+                  source={require("@/assets/images/mansion.png")}
+                  resizeMode="contain"
+                />
+                <Text>{filter}</Text>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+
+      <ScrollView style={styles.listingContainer}>
+        <View style={styles.paginationContainer}>
+          <TouchableHighlight style={styles.showMoreButton}>
+            <Text style={styles.showMoreButtonText}>Show More</Text>
+          </TouchableHighlight>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-export default index;
+export default Home;
