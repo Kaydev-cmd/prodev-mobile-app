8af895dabbe3b1f6ca54321638551b7ff48f358f
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-18 09:29:46 +0200

    fix: fix stack screen names to match file paths for proper routing

--- prodev-mobile-app-0x04/app/_layout.tsx
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -4,9 +4,9 @@ import { View } from "react-native";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="/" />
+      <Stack.Screen name="index" />
       <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
+      <Stack.Screen name="Sign" />
     </Stack>
   );
 }
